534054246a88e2841bb20eac86eb099a1aff1624
unknown <jrwilson5116@gmail.com>
2018-02-23 21:39:48 -0600

    replaces three for cleaner code that does the same job

--- caesar.py
+++ caesar.py
@@ -12,16 +12,9 @@ def alphabet_position(users_letter):
     for i in range(97,123):
         alphabet.append(chr(i))
 
-    upper_alphabet = []
-    for i in range(65, 91):
-        upper_alphabet.append(chr(i))
-
     for i in range(0,len(alphabet)):
-        if users_letter == alphabet[i]:
-            return i
-
-    for i in range(0,len(upper_alphabet)):
-        if users_letter == upper_alphabet[i]:
+        current = alphabet[i]
+        if users_letter == current.lower():
             return i
 
 def rotate_character(char, rot):
@@ -30,9 +23,9 @@ def rotate_character(char, rot):
     for i in range(97,123):
         alphabet.append(chr(i))
 
-    upper_alphabet = []
-    for i in range(65, 91):
-        upper_alphabet.append(chr(i))
+    # upper_alphabet = []
+    # for i in range(65, 91):
+    #     upper_alphabet.append(chr(i))
 
     if char.isalpha():
         new_position = alphabet_position(char) + int(rot)
@@ -44,7 +37,8 @@ def rotate_character(char, rot):
                 new_position = new_position + 26
 
     if char.isupper():
-        return upper_alphabet[new_position]
+        current = alphabet[new_position]
+        return current.upper()
     elif char.islower():
         return alphabet[new_position]
     else:
